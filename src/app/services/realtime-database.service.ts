9d163de9ffbe4081f228f7c4a62b390ad80b0bbb
Mohanraj SC <60479297+Mohanraj237@users.noreply.github.com>
2022-02-19 22:17:32 +0530

    Add files via upload

---
+++ src/app/services/realtime-database.service.ts
@@ -0,0 +1,18 @@
+
+import { Injectable } from '@angular/core';
+import { AngularFireDatabase, AngularFireList } from '@angular/fire/compat/database';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RealtimeDatabaseService {
+
+  imageDetailList!: AngularFireList<any>;
+
+  constructor(private firebase: AngularFireDatabase) { }
+
+  getImageDetailList() {
+    this.imageDetailList = this.firebase.list('Card1');
+  }
+}
